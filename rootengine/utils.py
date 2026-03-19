77657f80b740ae4e8ab6f7226a22df6f699560a6
zimvir <zimvir@qq.com>
2026-03-19 22:50:48 +0800

    utils里加了个返回UTC ISO时间的函数

--- rootengine/utils.py
+++ rootengine/utils.py
@@ -9,3 +9,16 @@ def oa(role, content):
     return {'role': role, 'content': content}
 def oat(role, content,tool_calls):
     return {'role': role, 'content': content,'tool_calls': tool_calls}
+
+
+
+
+#返回 UTC 的 ISO 格式的时间
+from datetime import datetime, timezone
+def get_iso_timestamp(microseconds=True):
+    """自定义是否包含微秒"""
+    if microseconds:
+        return datetime.now(timezone.utc).isoformat(timespec='microseconds').replace('+00:00', 'Z')
+    else:
+        return datetime.now(timezone.utc).isoformat(timespec='seconds').replace('+00:00', 'Z')
+
